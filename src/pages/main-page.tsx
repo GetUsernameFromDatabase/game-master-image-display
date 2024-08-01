3b276ab20cf92bdb39fbeaa214b8529976c62045
Ryan Murulo <rkruberg@gmail.com>
2024-08-02 01:00:55 +0300

    feat: navigation to image view link

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -11,9 +11,12 @@ import { DataDisplayChooser } from '@/components/data-display-chooser';
 import { ImageTable } from '@/components/image-table';
 import { useAppDispatch } from '@/app/store/hooks';
 import { setColumnSettings } from '@/app/store/slices/table-slice';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
+import { Image } from 'lucide-react';
 
 export default function MainPage() {
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
   const [columnChooserDialogueOpen, setColumnChooserDialogueOpen] =
     useState(false);
@@ -29,6 +32,15 @@ export default function MainPage() {
         <main className='grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8'>
           <div className='flex items-center'>
             <div className='ml-auto flex items-center gap-2'>
+              <Link to='view-image' target='_blank'>
+                <Button size='sm' variant='outline' className='h-8 gap-1'>
+                  <Image className='h-3.5 w-3.5' />
+                  <span className='sr-only sm:not-sr-only sm:whitespace-nowrap'>
+                    Image View Window
+                  </span>
+                </Button>
+              </Link>
+
               <ImportDialogue onSuccess={handleImportSuccess}></ImportDialogue>
             </div>
           </div>
@@ -47,7 +59,7 @@ export default function MainPage() {
             <DataDisplayChooser
               onFinish={(boxes) => {
                 console.log(boxes);
-                dispatch(setColumnSettings(boxes))
+                dispatch(setColumnSettings(boxes));
                 setColumnChooserDialogueOpen(false);
               }}
             ></DataDisplayChooser>
